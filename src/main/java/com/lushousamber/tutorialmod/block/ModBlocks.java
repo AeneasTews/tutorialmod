a90aa1af03c328f3d0e44b5e8fdb79422235cc47
AeneasTews <Aeneas.tews@gmail.com>
2022-06-04 19:32:20 +0200

    citrine block

--- src/main/java/com/lushousamber/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/lushousamber/tutorialmod/block/ModBlocks.java
@@ -2,15 +2,16 @@ package com.lushousamber.tutorialmod.block;
 
 import com.lushousamber.tutorialmod.TutorialMod;
 import com.lushousamber.tutorialmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.openjdk.nashorn.internal.ir.Block;
 
 import java.util.function.Supplier;
 
