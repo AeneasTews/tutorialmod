e03a34717e36734feef2c3ca126afa16d310f8d6
AeneasTews <Aeneas.tews@gmail.com>
2022-06-05 12:02:19 +0200

    Cucumber food

--- src/main/java/com/lushousamber/tutorialmod/item/ModFoods.java
+++ src/main/java/com/lushousamber/tutorialmod/item/ModFoods.java
@@ -5,7 +5,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties CUCUMBER = (new FoodProperties.Builder()).nutrition(4)
+    public static final FoodProperties CUCUMBER = (new FoodProperties.Builder()).nutrition(7)
             .saturationMod(0.2F).effect(new MobEffectInstance(MobEffects.LEVITATION, 6000, 1), 0.1f).build();
 
 }
